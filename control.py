d688fd9a8d669dfbba086b50ff236bd47421355a
LAIKA\maxvd_000 <maxvdree@hotmail.com>
2021-01-23 00:14:53 +0100

    Expand ui

--- control.py
+++ control.py
@@ -1,6 +1,7 @@
 """Controller"""
 import sys
 import random
+import re
 
 from PySide2 import QtCore as qc
 from PySide2 import QtWidgets as qg
@@ -12,8 +13,9 @@ class Controller(qc.QObject):
 
     def __init__(self, examinator):
         qc.QObject.__init__(self)
-        self.gui = window.GUI()
+        self.gui = window.GUI("'Bedrijfsinformatiesystemen'")
         self.exam = examinator
+        self.state = "question"
 
     def show_window(self):
         self.connect_button()
@@ -23,17 +25,29 @@ class Controller(qc.QObject):
         self.gui.check_button.clicked.connect(self.on_check_clicked)
 
     def on_check_clicked(self):
-        self.evaluate_answer()
-        self.update_gui()
+        if self.state == "question":
+            # switch state to 'evaluation'
+            self.state = "evaluation"
+            answer = self.gui.txtfield.text()
+            self.exam.eval(answer)
+            self.update_gui(self.exam.result)
+        else:
+            # switch state to 'question'
+            self.state = "question"
+            self.exam.update()
+            self.update_gui()
 
-    def update_gui(self):
+    def update_gui(self, result=None):
         print('-- Updating gui...\n')
-        self.gui.replace_question(self.exam.question)
-        self.gui.reset_textfield()
 
-    def evaluate_answer(self):
-        answer = self.gui.txtfield.text()
-        result = self.exam.eval(answer)
+        if self.state == "question": 
+            self.gui.enable_textfield(True)   
+            self.gui.replace_question(self.exam.question)
+            self.gui.reset_textfields()
+        else:
+            self.gui.enable_textfield(False)
+            self.gui.set_correctiontext(self.exam.correct_answer)
+            self.gui.replace_result(result)
 
 
 class Examinator():
@@ -43,6 +57,9 @@ class Examinator():
         self.correct_answer = ""
         self.curriculum = {}
         self.questions = []
+        self.minimum_answer = 3
+        self.result = None
+        self.match_threshold = 25.0
 
     def init_curriculum(self):
         self.curriculum = content.main()
@@ -56,37 +73,46 @@ class Examinator():
             return list(self.curriculum.values())
 
     def eval(self, answer):
-        print("-- Evaluating answer:", answer)
+        print("-- Evaluating answer:", answer, "to question", 
+              self.question)
 
         if self.match(answer, self.correct_answer):
             print("-- The answer was correct!")
             self.questions.remove(self.question)
-            result = True
+            self.result = True
         else:
             print("-- Incorrect. The correct answer is:\n",
                   self.correct_answer)
             self.questions.append(self.question)
-            result = False
+            self.result = False
 
-        self.update_exam()
-
-        return result
-
-    def update_exam(self):
+    def update(self):
         self.question = self.questions[random.randint(
                             0, len(self.questions)-1)]
 
         self.correct_answer = self.curriculum[self.question]
 
-    def match(self, answer, definition):
-        return answer == definition            
+    def match(self, answer, correct_answer):
+        correct_answer = correct_answer.lower()
+        answer = answer.lower()
+        print("Your answer:", answer)
+
+        word_matches = [word.lower() in correct_answer.split(" ") 
+                        for word in answer.split(" ")]
+
+        print("word matches:", word_matches)
+        match_percentage = sum(word_matches) / len(word_matches) * 100.0
+        print("match percentag:", match_percentage)
+
+        return (match_percentage >= self.match_threshold 
+                and len(word_matches) >= self.minimum_answer)
 
 
 def main():
     # Examinator
     exam = Examinator()
     exam.init_curriculum()
-    exam.update_exam()
+    exam.update()
 
     # GUI
     app = qg.QApplication(sys.argv)
--- window.py
+++ window.py
@@ -1,5 +1,6 @@
 from PySide2 import QtCore as qc
-from PySide2 import QtWidgets as qg
+from PySide2 import QtWidgets as qw
+from PySide2 import QtGui as qg
 
 import sys
 import gui_utils as utils
@@ -7,39 +8,97 @@ import os.path
 #import overhoorscript_v01 as content
         
 
-class GUI(qg.QDialog):
-    def __init__(self):
-        qg.QDialog.__init__(self)
-        self.setWindowTitle('Overhoorscript')
+class GUI(qw.QDialog):
+
+    green_style = "QLabel { background-color : green; color : white; }"
+    orange_style = "QLabel { background-color : orange; color : black; }"
+    arial_font = qg.QFont("Arial", 16) 
+    sentence_width = 130
+
+    def __init__(self, title="subject placeholder"):
+        qw.QDialog.__init__(self)
 
         # window setup
+        self.setWindowTitle("Overhoorscript: {}".format(title))
         self.setWindowFlags(qc.Qt.WindowStaysOnTopHint)
-        self.setMinimumWidth(500)
+        self.setMinimumWidth(1400)
         
         # main layout
-        self.mainLayout = qg.QVBoxLayout()
+        self.mainLayout = qw.QVBoxLayout()
         self.setLayout(self.mainLayout)
         
         # question widgets
-        self.question = utils.Label_custom('')
-        self.txtfield = qg.QLineEdit()
-        
-        # add to main layout
-        self.mainLayout.addWidget(self.question)
-        self.mainLayout.addWidget(self.txtfield)
+        self.content_layout = qw.QVBoxLayout()
+        self.question = qw.QLabel("")
+        self.question.setFont(self.arial_font)
+        self.txtfield = qw.QLineEdit()
+        self.txtfield.setFont(self.arial_font)
+        self.resulttext = qw.QLabel("")
+        self.resulttext.setFont(self.arial_font)
+        self.spacer = qw.QLabel("")
+        self.sentence_label = None
+
+        # correction layout
+        self.correction_layout = qw.QVBoxLayout()
+
+        # add to content layout
+        self.content_layout.addWidget(self.question)
+        self.content_layout.addWidget(self.txtfield)
+        self.content_layout.addWidget(self.spacer)
+        self.content_layout.addWidget(self.resulttext)
+        self.content_layout.addWidget(self.spacer)
+        self.content_layout.addLayout(self.correction_layout)
+        self.content_layout.addWidget(self.spacer)
         
         # main layout button
         self.check_button = utils.PushButton_custom('Check', 'SansSerif', 12, [60, 140, 60])
         
         # add makePlayblast button to main layout
+        self.mainLayout.addLayout(self.content_layout)
         self.mainLayout.addWidget(self.check_button)
 
     def replace_question(self, question):
         print('question:', question)
         self.question.setText(question)
 
-    def reset_textfield(self):
-        self.txtfield.setText("")
+    def set_correctiontext(self, correctiontext):
+        self.check_button.setText("Next Question")
+
+        sentence = ""
+        for word in correctiontext.split(" "):
+            sentence += word + " "
+            if len(sentence) >= self.sentence_width:
+                self.sentence_label = utils.Label_custom(sentence)
+                self.sentence_label.setFont(self.arial_font)
+                self.correction_layout.addWidget(self.sentence_label)
+                sentence = ""
+
+        self.sentence_label = utils.Label_custom(sentence)
+        self.sentence_label.setFont(self.arial_font)
+        self.correction_layout.addWidget(self.sentence_label)
+
+    def reset_textfields(self):
+        self.check_button.setText("Check")
+        self.txtfield.clear()
+        self.txtfield.setFocus()
+        self.resulttext.setText("")
+        self.resulttext.setStyleSheet("")
+        self.clear_layout(self.correction_layout)
+
+    def enable_textfield(self, lockstate):
+        self.txtfield.setEnabled(lockstate)
+
+    def clear_layout(self, layout):
+        for i in reversed(range(layout.count())):
+            layout.itemAt(i).widget().setParent(None)
+
+    def replace_result(self, correct):
+        if type(correct) is bool and correct:
+            self.resulttext.setText("Correct!")
+            self.resulttext.setStyleSheet(self.green_style)
+        else:
+            self.resulttext.setText("Incorrect.")
+            self.resulttext.setStyleSheet(self.orange_style)
 
     def closeEvent(self, event=None):
         self.close()
@@ -51,10 +110,11 @@ class GUI(qg.QDialog):
 
 def main():
     gui = GUI()
+    gui.show()
     return gui
 
 if __name__ == '__main__':
-    app = qg.QApplication(sys.argv)
+    app = qw.QApplication(sys.argv)
     gui = main()
     app.exec_()
 
