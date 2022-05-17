bca3e69ac5888ae3285c536eb7e8d04a8ddafc1c
Max van der Ree <maxvdree@hotmail.com>
2022-05-17 23:07:25 +0200

    Update for philospohy of science and ethics

--- content.py
+++ content.py
@@ -221,7 +221,7 @@ def get_data_dict(filepath, subject):
     return rawdata
 
 
-def get_content(subject=None, subject_dir=r"C:\Users\Max\OneDrive\Documenten\Cognitive modeling\content_bronnen"):
+def get_content(subject=None, subject_dir=r"D:\OneDrive\Documenten\Philosophy of Science and Ethics\content_bronnen"):
     content = {}
     work_dir = pathlib.Path(__file__).parent.absolute()
     if not subject_dir:
@@ -251,7 +251,7 @@ if __name__ == "__main__":
         print("Change 'overwrite' to 'True' to overwrite. You sure though? This will reset the database.")
         exit()
 
-    subject = "Cognitive Modeling"
+    subject = "Philosophy of Science and Ethics"
     content = get_content(subject)
     print("Chapters:", list(content.keys()))
     no_of_questions = 0
